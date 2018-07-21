1186c79ca531c092c6f46d0c4facbd8a2634228e
frkntony <tony.elistratov@gmail.com>
2018-07-21 18:57:14 +0300

    Pixabay API is connected. Search is working.

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import TextField from 'material-ui/TextField'
 import SelectField from 'material-ui/SelectField'
 import MenuItem from 'material-ui/MenuItem'
+import axios from 'axios'
+import ImageResults from '../image-results/ImageResults'
 
 class Search extends Component {
   state = {
@@ -12,8 +14,19 @@ class Search extends Component {
     images: []
   }
 
+  onTextChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value}, () => {
+      axios.get(`${this.state.apiUrl}/?key=${this.state.apiKey}&q=${this.state.searchText}&image_type=photo&per_page=${this.state.amount}&safesearch=true`)
+      .then(res => this.setState({images: res.data.hits}))
+      .catch(err => console.log(err))
+    })
+  }
+
+
+  onAmountChange= (e, index, value) => this.setState({ amount:value })
 
   render() {
+    console.log(this.state.images);
     return (
       <div>
         <TextField 
@@ -23,6 +36,21 @@ class Search extends Component {
           floatingLabelText='Raid For Images!'
           fullWidth={true}
         />
+        <br />
+        <SelectField
+          name='amount'
+          floatingLabelText='Amount'
+          value={this.state.amount}
+          onChange={this.onAmountChange}
+        >
+          <MenuItem value={5} primaryText='5' />
+          <MenuItem value={10} primaryText='10' />
+          <MenuItem value={15} primaryText='15' />
+          <MenuItem value={30} primaryText='30' />
+          <MenuItem value={50} primaryText='50' />
+        </SelectField>
+        <br />
+        {this.state.images.length > 0 ? (<ImageResults images={this.state.images} />) : null }
         
       </div>
     )
